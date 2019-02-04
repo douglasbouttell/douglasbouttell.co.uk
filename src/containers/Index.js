c6df6876f9649326999f83c0b4fa58a81038bb90
Douglas Bouttell <douglas.bouttell@gmail.com>
2019-02-04 12:07:44 +0000

    Create a section in my index for soft-dev-should-know pages

--- src/containers/Index.js
+++ src/containers/Index.js
@@ -6,15 +6,41 @@ import convert from 'htmr'
 import './Index.css'
 //
 
+const getTime = post => new Date(post.meta.date).getTime();
+const byDate = (a, b) => getTime(b) - getTime(a);
+
+const SlugScraperHOC = (regex, title) => ({posts}) => {
+  const id = title.toLowerCase()
+    .replace(/[^a-z0-9 ]/g, '')
+    .replace(/ +/g, '-');
+
+  const slug = x => x.meta.slug;
+  const filtered_posts = posts
+    .filter(post => regex.test(slug(post)))
+    .sort(byDate);
+
+  return filtered_posts && (<aside>
+    <h3>{title}</h3>
+    <ul id={`${id}-posts`} className="posts">
+      {filtered_posts.map(post =>
+        <li key={post.path}>
+          <Link to={post.path}>{post.meta.title}</Link>
+        </li>)}
+    </ul>
+  </aside>)
+};
+
+const SoftDevShouldKnowSlug = SlugScraperHOC(
+  /^soft-dev-should-know-.*/,
+  'Software Developers Should Know'
+);
+
 const RecentPosts = ({posts, limit = 5}) => {
-  const getTime = post => new Date(post.meta.date).getTime();
-  const recent_posts = posts
-  .sort((a, b) => getTime(b) - getTime(a))
-  .slice(0, limit);
+  const recent_posts = posts.sort(byDate).slice(0, limit);
 
   return <aside>
-    <h2>Recent Posts</h2>
-    <ul className="recent-posts">
+    <h3>Recent Posts</h3>
+    <ul id="recent-posts" className="posts">
       {recent_posts.map(post =>
         <li key={post.path}>
           <Link to={post.path}>{post.meta.title}</Link>
@@ -36,7 +62,10 @@ const Index = (props) => {
         }
       </Head>
       <div className="container">
-        {convert(html)}
+        <section id={"index-content"}>
+          {convert(html)}
+        </section>
+        <SoftDevShouldKnowSlug posts={meta.posts}/>
         <RecentPosts posts={meta.posts}/>
       </div>
     </React.Fragment>
