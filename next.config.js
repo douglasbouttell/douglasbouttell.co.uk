b57342524bab7b85290e9867eae273b7892a9bbd
Douglas Bouttell <douglasbouttell@gmail.com>
2023-04-16 11:58:00 +0100

    Do not optimize images

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    output: 'export',
-}
-  
-module.exports = nextConfig
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
