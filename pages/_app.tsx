654a3e5dfa6c68af78b5d81e6adb121909e92106
Douglas Bouttell <douglasbouttell@gmail.com>
2023-04-16 15:46:27 +0100

    New look

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,56 @@
 import type { AppProps } from 'next/app';
 
 import 'normalize.css/normalize.css';
-import '~/styles/app.css';
+
+import { Amaranth, Open_Sans } from 'next/font/google';
+
+const openSans = Open_Sans({
+  weight: ['400', '500', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+});
+
+const amaranth = Amaranth({
+  weight: ['700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html,
+        body,
+        body > div:first-child,
+        div#__next,
+        div#__next > div {
+          min-height: 100vh;
+          padding: 0;
+        }
+
+        body {
+          font-size: 12pt;
+          font-family: ${openSans.style.fontFamily};
+          font-weight: 400;
+          color: #333;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+          font-family: ${amaranth.style.fontFamily};
+          font-weight: 700;
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,60 @@
-import Image from 'next/image';
-import css from '~/styles/index.module.css';
-import title from '~/img/title.svg';
+import Head from 'next/head';
+import { Russo_One } from 'next/font/google';
+
+const russoOne = Russo_One({
+  weight: ['400'],
+  style: ['normal'],
+  subsets: ['latin'],
+});
 
 export default function HomePage() {
   return (
-    <div className={css.container}>
-      <div>
-        <main>
-          <p>
-            <Image src={title} alt="Douglas Bouttell" />
-          </p>
-          <p>
-            Software Engineer <span role="presentation">|</span> Sci-fi Nerd{' '}
-            <span role="presentation">|</span> Tech Fan{' '}
-            <span role="presentation">|</span> Foodie
-          </p>
-        </main>
+    <>
+      <Head>
+        <title>Douglas Bouttell</title>
+      </Head>
+      <style jsx>{`
+        .container {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+
+          background: #7f00ff;
+          background: linear-gradient(to top, #e100ff, #7f00ff);
+
+          height: 100%;
+        }
+
+        .container p {
+          text-align: center;
+          margin: 0.25rem;
+          padding: 0.25rem;
+        }
+
+        h1 {
+          -webkit-text-stroke: 2px black;
+          text-shadow: 4px 4px 0px black;
+          color: white;
+          font-size: 6rem;
+          margin: 0;
+          text-align: center;
+        }
+
+        p {
+          -webkit-text-stroke: 1px black;
+          font-weight: 700;
+          font-size: 2rem;
+          color: white;
+        }
+      `}</style>
+      <div className="container">
+        <div>
+          <main style={{ position: 'relative' }}>
+            <h1 className={russoOne.className}>Douglas Bouttell</h1>
+            <p>Software Engineer, Sci-fi Nerd, Tech Fan, Foodie</p>
+          </main>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
